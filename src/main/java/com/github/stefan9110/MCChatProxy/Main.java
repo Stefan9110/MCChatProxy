d52513402bd590c6f11f40053cdc1e0ca144e78d
stefan911 <danielstefan.halasz@gmail.com>
2022-08-22 13:43:42 +0300

    Initial commit Added:  - Gradle project, build.gradle (shadowJar implementation)  - Main class

---
+++ src/main/java/com/github/stefan9110/MCChatProxy/Main.java
@@ -0,0 +1,7 @@
+package com.github.stefan9110.MCChatProxy;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi this is here just to test");
+    }
+}
